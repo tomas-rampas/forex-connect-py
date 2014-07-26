87553addfc66559ee805c43e6d6dae226bea37ee
tomas-rampas <tomas.rampas@gmail.com>
2014-07-26 21:49:17 +0200

    void * > PyObject *

--- src/ForexConnectClient/forex.connect/IO2GRow.cpp
+++ src/ForexConnectClient/forex.connect/IO2GRow.cpp
@@ -5,7 +5,10 @@ using namespace boost::python;
 class IO2GRowWrap : public IO2GRow, public wrapper < IO2GRow >
 {
 public:
-	const void* getCell(int column){ return this->get_override("getCell")();}
+	const void* getCell(int column)
+	{ 
+		return this->get_override("getCell")(column);
+	}
 	bool isCellChanged(int column){ return this->get_override("isCellChanged")();}
 	IO2GTableColumnCollection* columns(){ return this->get_override("columns")();}
 	O2GTable getTableType(){ return this->get_override("getTableType")();}
@@ -15,7 +18,7 @@ public:
 void export_IO2GRow()
 {
 	class_<IO2GRowWrap, bases<IAddRef>, boost::noncopyable>("IO2GRow", no_init)
-		//.def("getCell", pure_virtual(&IO2GRow::getCell), return_value_policy<reference_existing_object>())
+		.def("getCell", pure_virtual((PyObject *(IO2GRow::*)(int))(&IO2GRow::getCell)))
 		.def("isCellChanged", pure_virtual(&IO2GRow::isCellChanged))
 		.def("columns", pure_virtual(&IO2GRow::columns), return_value_policy<reference_existing_object>())
 		.def("getTableType", pure_virtual(&IO2GRow::getTableType))
