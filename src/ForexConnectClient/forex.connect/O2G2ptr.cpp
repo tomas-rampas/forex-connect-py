3587d95893b3a252a4509fd4b43c7574faa6f819
tomas-rampas <tomas.rampas@gmail.com>
2014-07-29 21:46:08 +0200

    continuing on template smart ptr fun

--- src/ForexConnectClient/forex.connect/O2G2ptr.cpp
+++ src/ForexConnectClient/forex.connect/O2G2ptr.cpp
@@ -4,14 +4,19 @@
 
 using namespace boost::python;
 
-class IO2GSessionPtr: public O2G2Ptr<IO2GSession>
+class IO2GSessionPtr : public O2G2Ptr < IO2GSession >
 {
 };
 
 void export_O2G2PteredClasses()
 {
 	class_<IO2GSessionPtr>("IO2GSessionPtr", init<>())
-		.def("Release", &IO2GSessionPtr::Release)
-		.def("Detach", (IO2GSession*(IO2GSessionPtr::*)())(&IO2GSessionPtr::Detach), return_value_policy<reference_existing_object>())
+		.def(init<IO2GSessionPtr>())
+		.def("release", &IO2GSessionPtr::Release)
+		.def("detach", (IO2GSession*(IO2GSessionPtr::*)())(&IO2GSessionPtr::Detach), return_value_policy<reference_existing_object>())
+		.def("assign", &IO2GSessionPtr::operator=, return_value_policy<reference_existing_object>())
+		.def("__eq__", &IO2GSessionPtr::operator==)
+		.def("__lt__", &IO2GSessionPtr::operator<)
+		.def("__not__", &IO2GSessionPtr::operator!)
 		;
 };
