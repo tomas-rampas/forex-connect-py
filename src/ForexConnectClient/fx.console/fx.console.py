0d9ab961e69c5bbef9647516ebdd3bc5dc33b3cf
tomas-rampas <tomas.rampas@gmail.com>
2014-08-24 21:21:12 +0200

    pyconsole

--- src/ForexConnectClient/fx.console/fx.console.py
+++ src/ForexConnectClient/fx.console/fx.console.py
@@ -1,24 +1,8 @@
 import forexconnect as fx
 from _ctypes import *
 
-class SessionStatusListener(fx.IO2GSessionStatus):
-    def __init__(self, session, printSubsessions, sessionID, pin):
-        self.session = session
-        self.printSubsessions = printSubsessions
-        self.sessionID = sessionID
-        self.pin = pin
-        session.addRef()
-
-    def __del__(self):
-        session.release()
-    
-    def onSessionStatusChanged(self):
-        pass
-    def onLoginFailed(self):
-        pass
-
 session = fx.CO2GTransport.createSession()
-status = SessionStatusListener(session, False, "", "")
+status = fx.SessionStatusListener(session, False, "", "")
 
 session.subscribeSessionStatus(status)
 
