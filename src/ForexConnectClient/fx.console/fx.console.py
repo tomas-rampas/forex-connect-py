d640c62623dd2b49b2d126a2e21718262ffea1cb
tomas-rampas <tomas.rampas@gmail.com>
2014-08-27 20:45:59 +0200

    local settings for account no and pwd have been added

--- src/ForexConnectClient/fx.console/fx.console.py
+++ src/ForexConnectClient/fx.console/fx.console.py
@@ -1,11 +1,12 @@
 import forexconnect as fx
+from settings import ACCOUNT, PWD
 
 session = fx.CO2GTransport.createSession()
 status = fx.SessionStatusListener(session, False, "", "")
 
 session.subscribeSessionStatus(status);
 
-session.login("{ACCOUNT}", "{PWD}", "http://www.fxcorporate.com/Hosts.jsp", "Demo")
+session.login(ACCOUNT, PWD, "http://www.fxcorporate.com/Hosts.jsp", "Demo")
 
 if status.waitEvents() and status.isConnected():
     print "ForexConnect client Connected"
